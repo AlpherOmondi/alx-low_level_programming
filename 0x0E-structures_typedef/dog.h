0e348b4d60ec00794a9f2f926e6406026476f738
AlpherOmondi <alphaomondi001@gmail.com>
2022-05-18 05:54:03 -0700

    struct of 3 data types

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * dog structure
+ * @name-dogs name
+ * @age-dogs age
+ * @owner-dog owner
+ */
+typedef struct dog{
+char *name;
+float age;
+char *owner;
+}
+dog_t;
+#endif
+ 
