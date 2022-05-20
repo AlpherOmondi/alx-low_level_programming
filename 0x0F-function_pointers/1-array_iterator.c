ae0203b32d50783d4df833363614cbafbb03e76c
AlpherOmondi <alphaomondi001@gmail.com>
2022-05-19 23:17:12 -0700

    executes a function parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,20 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name.
- * @name: pointer to varable.
- * @f: function pointer.
+ * array_iterator - executes a function parameter on each element of an array.
+ *@array: pointer to integer array.
+ *@size:  size of the array.
+ *@action: a pointer to the function you need to use.
  *
  * Return: Nothing.
  */
-void print_name(char *name, void (*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (f != NULL)
-f(name);
+size_t i;
+
+if (action != NULL && array != NULL)
+{
+for (i = 0; i < size; i++)
+action(array[i]);
+}
 }
