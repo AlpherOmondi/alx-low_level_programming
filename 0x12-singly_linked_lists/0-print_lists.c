b5c8e7ea6b482ce63efc49a24960baccc2ec2b7f
AlpherOmondi <alphaomondi001@gmail.com>
2022-05-24 12:07:23 -0700

    prints elements of alist

--- 0x12-singly_linked_lists/0-print_lists.c
+++ 0x12-singly_linked_lists/0-print_lists.c
@@ -1,23 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: pointer to struct.
+ * print_list - prints all the elements of a list_t list
  *
- * Return:  the number of nodes.
+ * @h: list to browse
+ *
+ * Return: size of the list
  */
 size_t print_list(const list_t *h)
 {
-int count = 0;
-
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-count++;
-}
-return (count);
+	if (!h)
+		return (0);
+	if (h->next == NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		return (1);
+	}
+	else
+	{
+		if (h->str == NULL)
+			printf("[0] %s\n", "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		return (print_list(h->next) + 1);
+	}
 }
